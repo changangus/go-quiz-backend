651f7d4d5cbd49f700cc60dba34527c3f3dac202
Angus Chang <changangus2@gmail.com>
2025-04-21 20:52:49 -0400

    refactor: move main.go

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run()
+
+	fmt.Println("Server is running on port 8080 with Gin")
+}
